8801e344fbf42dcf6162d71d9c2238a97369ffbd
alex <apriem@tubeness.nl>
2014-02-03 00:05:04 +0100

    rcfilter: add dual encoders.

--- rcfilter/main.c
+++ rcfilter/main.c
@@ -28,9 +28,11 @@
 		  
 int main(void)
 {	
-	int8_t vol, changed;
+	int8_t vol, bal, changed1, changed2;
 	char buf[10];
 	
+	vol=20;
+	bal=20;
 	uart_init();
 	uart_puts ("\r\nreset -- init\r\n");
 	//				sh_cp    st_cp    ds
@@ -56,7 +58,7 @@ int main(void)
 
 	//void lcd_setup (uint8_t rs, uint8_t rw, uint8_t enable1, uint8_t enable2,
 	//			uint8_t db0, uint8_t db1, uint8_t db2, uint8_t db3 ) 
-	 lcd_setup (0, P_PA2, P_PA4, P_PA0);			  
+	lcd_setup (0, P_PA2, P_PA4, P_PA0);			  
 	 
 			  //lcd_setup  (uint8_t chip,  uint8_t strobe, uint8_t clock, uint8_t io)
 	lcd_setup_info (0, HD44780, 20, 2);
@@ -65,23 +67,22 @@ int main(void)
 	lcd_init (0, LCD_DISP_ON);
 	
 	encoder_setup (0, P_PC0, P_PC2);
-	
+	encoder_setup (1, P_PC1, P_PC3);
 	
 
-	
-	
+	lcd_printf (0, "volume: %d:%d",vol,bal);	
+	 
 	uart_printf ("start loop \r\n");
 	
 	for (;;) {
-		changed=encoder_poll_range (0, &vol, 0,100,1);
-		//uart_printf ("%d %d\r\n",vol, changed);
+		changed1=encoder_poll_range (0, &vol, 0,100,1);
+		changed2=encoder_poll_range (1, &bal, 0,100,1);
+		uart_printf ("%d %d %d %d\r\n",vol, bal, changed1, changed2);
 		
-		if (changed==TRUE) {
+		if ((changed1==TRUE) || (changed2==TRUE)) {
 			lcd_clrscr (0);
-			lcd_puts (0, "volume: ");
-			itoa(vol, buf,10);    
-			lcd_puts (0, buf);
-			hc595_putc (0,vol);
+			lcd_printf (0, "volume: %d:%d",vol,bal);
+			//hc595_putc (0,vol);
 			}		
 	}
  return 0;
